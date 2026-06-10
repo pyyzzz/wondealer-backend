515437e5192cb8ebe3b17b848b3034e7080cc275
hmbyun3212 <hmbyun3212@gmail.com>
2026-06-10 15:26:01 +0900

    설계서에 따른 전체적인 구조 수정 (이름수정이 주)

--- src/main/java/com/wondealer/entity/ItemType.java
+++
@@ -1,8 +0,0 @@
-package com.wondealer.entity;
-
-public enum ItemType {
-    ITEM,       // 아이템
-    MONEY,      // 게임머니 (CURRENCY보다 설계서 뉘앙스 및 통상적 명칭에 부합)
-    ACCOUNT,    // 계정
-    ETC         // 기타 (설계서에 명시된 '기타' 카테고리 방어용)
-}
