ca5b3ffb257fdfc48620f9700d6833ed78157769
pyyzzz <kooor20089@gmail.com>
2026-06-16 00:11:49 +0900

    AuctionListResDto gameName 추가 instantBuyPrice 추가

--- src/main/java/com/wondealer/dto/response/AuctionListResDto.java
+++ src/main/java/com/wondealer/dto/response/AuctionListResDto.java
@@ -4,7 +4,6 @@ import com.wondealer.entity.Auction;
 import com.wondealer.entity.Item;
 import lombok.Builder;
 import lombok.Getter;
-
 import java.time.LocalDateTime;
 
 @Getter
@@ -14,9 +13,11 @@ public class AuctionListResDto {
     private Long auctionId;
     private Long itemId;
     private String itemTitle;
+    private String gameName;        // 어떤 게임 경매인지
     private String thumbnailImg;
     private Long startPrice;
     private Long currentPrice;
+    private Long instantBuyPrice;   // 즉시 낙찰가 (nullable)
     private Integer bidCount;
     private LocalDateTime endTime;
     private String status;
@@ -31,9 +32,11 @@ public class AuctionListResDto {
                 .auctionId(auction.getId())
                 .itemId(item.getId())
                 .itemTitle(item.getTitle())
+                .gameName(item.getGameCategory().getGame().getGameName())
                 .thumbnailImg(thumbnailImg)
                 .startPrice(auction.getStartPrice())
                 .currentPrice(auction.getCurrentPrice())
+                .instantBuyPrice(auction.getInstantBuyPrice())
                 .bidCount(auction.getBidCount())
                 .endTime(auction.getEndTime())
                 .status(auction.getStatus())
